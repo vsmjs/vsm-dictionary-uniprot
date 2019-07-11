68572a1e0aad6fe0eb518513a18d409a5ab864f5
john <bblodfon@gmail.com>
2019-07-11 13:55:21 +0100

    added test files for live testing Uniprot's REST API

---
+++ test/getEntries.test.js
@@ -0,0 +1,28 @@
+/**
+ * File used to quick test the `getEntries` function of
+ * `DictionaryUniprot.js`
+ */
+
+const DictionaryUniprot = require('../src/DictionaryUniprot');
+
+const dict = new DictionaryUniprot({log: true});
+
+dict.getEntries({
+  filter: {
+    id: [
+      /*'https://www.uniprot.org/uniprot/P52413',
+      'https://www.uniprot.org/uniprot/P53142',
+      'https://www.uniprot.org/uniprot/P12345',
+      'https://www.uniprot.org/uniprot/P05067'*/
+    ]},
+  //sort: 'str',
+  page: 1,
+  perPage: 5
+}, (err, res) => {
+  if (err) console.log(JSON.stringify(err, null, 4));
+  else {
+    console.log(JSON.stringify(res, null, 4));
+    console.log('\n#Results: ' + res.items.length);
+  }
+}
+);
---
+++ test/getEntryMatchesForString.test.js
@@ -0,0 +1,18 @@
+/**
+ * File used to quick test the `getEntryMatchesForString` function of
+ * `DictionaryUniprot.js`
+ */
+
+const DictionaryUniprot = require('../src/DictionaryUniprot');
+
+const dict = new DictionaryUniprot({log: true});
+
+dict.getEntryMatchesForString('tp53', { page: 1, perPage: 20 },
+  (err, res) => {
+    if (err) console.log(err);
+    else {
+      console.log(JSON.stringify(res, null, 4));
+      console.log('\n#Results: ' + res.items.length);
+    }
+  }
+);
